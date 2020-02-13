251a8c0a1b245f4afdcdda40a5be094c8bc5d496
Roman Karpov <poma.05@mail.ru>
2020-02-13 17:54:41 +0400

    init

---
+++ State/Context.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace State
+{
+    public class Context
+    {
+        private State state;
+        public Context(State state)
+        {
+            this.state = state;
+        }
+        public State State
+        {
+            get { return state; }
+            set
+            {
+                state = value;
+                Console.WriteLine("State: " + state.GetType().Name);
+            }
+        }
+        public void Request()
+        {
+            state.Handle(this);
+        }
+    }
+}
